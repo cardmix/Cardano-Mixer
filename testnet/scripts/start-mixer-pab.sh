e353050c60c05d05062264ab253d23df47f52932
cardmix <91214090+cardmix@users.noreply.github.com>
2022-04-09 01:31:39 +0200

    Script update

--- testnet/scripts/start-mixer-pab.sh
+++ testnet/scripts/start-mixer-pab.sh
@@ -4,9 +4,9 @@ cd ../..
 
 rm -f testnet/data/plutus-pab.db
 
-dist-newstyle/build/x86_64-linux/ghc-8.10.4.20210212/cardano-mixer-backend-0.1.0.0/x/cardano-mixer-pab/build/cardano-mixer-pab/cardano-mixer-pab \
+testnet/bin/cardano-mixer-pab \
  --config testnet/pab-config.yml migrate
   
-dist-newstyle/build/x86_64-linux/ghc-8.10.4.20210212/cardano-mixer-backend-0.1.0.0/x/cardano-mixer-pab/build/cardano-mixer-pab/cardano-mixer-pab \
+testnet/bin/cardano-mixer-pab \
   --config testnet/pab-config.yml webserver \
   --passphrase 1234567890
