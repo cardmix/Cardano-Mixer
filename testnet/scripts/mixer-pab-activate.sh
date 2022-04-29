c6d7015e6eb64aa1f17ccb686ccb6a67e609bb8c
cardmix <91214090+cardmix@users.noreply.github.com>
2022-04-30 00:00:24 +0200

    update mixer-pab-activate

--- testnet/scripts/mixer-pab-activate.sh
+++ testnet/scripts/mixer-pab-activate.sh
@@ -2,4 +2,5 @@
 
 cd ../..
 
-testnet/bin/cardano-mixer-pab-client startup
+$NODE_HOME/Cardano-Mixer/testnet/bin/cardano-mixer-pab-client dispense
+$NODE_HOME/Cardano-Mixer/testnet/bin/cardano-mixer-pab-client retrieve
