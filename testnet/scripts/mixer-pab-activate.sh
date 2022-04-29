ab0cb0a822a2771355cdaf95eb89b2a568b7617a
cardmix <91214090+cardmix@users.noreply.github.com>
2022-04-30 00:38:24 +0200

    update mixer-pab-activate

--- testnet/scripts/mixer-pab-activate.sh
+++ testnet/scripts/mixer-pab-activate.sh
@@ -2,5 +2,8 @@
 
 cd ../..
 
+export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig"
+
 /home/ec2-user/cardano-my-node/Cardano-Mixer/testnet/bin/cardano-mixer-pab-client dispense
 /home/ec2-user/cardano-my-node/Cardano-Mixer/testnet/bin/cardano-mixer-pab-client retrieve
