8d647b5d94d6173d2899a90042092011ad027564
cardmix <91214090+cardmix@users.noreply.github.com>
2022-04-29 23:53:49 +0200

    Add restart to mixer pab script

--- testnet/scripts/start-mixer-pab.sh
+++ testnet/scripts/start-mixer-pab.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-pid=$(pidof cardano-mixer-pab)
-killall -9 cardano-mixer-pab
-
 cd ../..
 
 rm -f testnet/data/plutus-pab.db
@@ -13,3 +10,237 @@ testnet/bin/cardano-mixer-pab \
 testnet/bin/cardano-mixer-pab \
   --config testnet/pab-config.yml webserver \
   --passphrase 1234567890
+  
+
+rm -f testnet/data/plutus-pab.db
+
+testnet/bin/cardano-mixer-pab \
+ --config testnet/pab-config.yml migrate
+  
+testnet/bin/cardano-mixer-pab \
+  --config testnet/pab-config.yml webserver \
+  --passphrase 1234567890
+  
+rm -f testnet/data/plutus-pab.db
+
+testnet/bin/cardano-mixer-pab \
+ --config testnet/pab-config.yml migrate
+  
+testnet/bin/cardano-mixer-pab \
+  --config testnet/pab-config.yml webserver \
+  --passphrase 1234567890
+  
+  
+rm -f testnet/data/plutus-pab.db
+
+testnet/bin/cardano-mixer-pab \
+ --config testnet/pab-config.yml migrate
+  
+testnet/bin/cardano-mixer-pab \
+  --config testnet/pab-config.yml webserver \
+  --passphrase 1234567890
+  
+rm -f testnet/data/plutus-pab.db
+
+testnet/bin/cardano-mixer-pab \
+ --config testnet/pab-config.yml migrate
+  
+testnet/bin/cardano-mixer-pab \
+  --config testnet/pab-config.yml webserver \
+  --passphrase 1234567890
+  
+  
+rm -f testnet/data/plutus-pab.db
+
+testnet/bin/cardano-mixer-pab \
+ --config testnet/pab-config.yml migrate
+  
+testnet/bin/cardano-mixer-pab \
+  --config testnet/pab-config.yml webserver \
+  --passphrase 1234567890
+  
+  
+rm -f testnet/data/plutus-pab.db
+
+testnet/bin/cardano-mixer-pab \
+ --config testnet/pab-config.yml migrate
+  
+testnet/bin/cardano-mixer-pab \
+  --config testnet/pab-config.yml webserver \
+  --passphrase 1234567890
+
+
+rm -f testnet/data/plutus-pab.db
+
+testnet/bin/cardano-mixer-pab \
+ --config testnet/pab-config.yml migrate
+  
+testnet/bin/cardano-mixer-pab \
+  --config testnet/pab-config.yml webserver \
+  --passphrase 1234567890
+  
+
+rm -f testnet/data/plutus-pab.db
+
+testnet/bin/cardano-mixer-pab \
+ --config testnet/pab-config.yml migrate
+  
+testnet/bin/cardano-mixer-pab \
+  --config testnet/pab-config.yml webserver \
+  --passphrase 1234567890
+  
+
+rm -f testnet/data/plutus-pab.db
+
+testnet/bin/cardano-mixer-pab \
+ --config testnet/pab-config.yml migrate
+  
+testnet/bin/cardano-mixer-pab \
+  --config testnet/pab-config.yml webserver \
+  --passphrase 1234567890
+  
+
+rm -f testnet/data/plutus-pab.db
+
+testnet/bin/cardano-mixer-pab \
+ --config testnet/pab-config.yml migrate
+  
+testnet/bin/cardano-mixer-pab \
+  --config testnet/pab-config.yml webserver \
+  --passphrase 1234567890
+  
+
+rm -f testnet/data/plutus-pab.db
+
+testnet/bin/cardano-mixer-pab \
+ --config testnet/pab-config.yml migrate
+  
+testnet/bin/cardano-mixer-pab \
+  --config testnet/pab-config.yml webserver \
+  --passphrase 1234567890
+  
+
+rm -f testnet/data/plutus-pab.db
+
+testnet/bin/cardano-mixer-pab \
+ --config testnet/pab-config.yml migrate
+  
+testnet/bin/cardano-mixer-pab \
+  --config testnet/pab-config.yml webserver \
+  --passphrase 1234567890
+  
+
+rm -f testnet/data/plutus-pab.db
+
+testnet/bin/cardano-mixer-pab \
+ --config testnet/pab-config.yml migrate
+  
+testnet/bin/cardano-mixer-pab \
+  --config testnet/pab-config.yml webserver \
+  --passphrase 1234567890
+  
+
+rm -f testnet/data/plutus-pab.db
+
+testnet/bin/cardano-mixer-pab \
+ --config testnet/pab-config.yml migrate
+  
+testnet/bin/cardano-mixer-pab \
+  --config testnet/pab-config.yml webserver \
+  --passphrase 1234567890
+  
+
+rm -f testnet/data/plutus-pab.db
+
+testnet/bin/cardano-mixer-pab \
+ --config testnet/pab-config.yml migrate
+  
+testnet/bin/cardano-mixer-pab \
+  --config testnet/pab-config.yml webserver \
+  --passphrase 1234567890
+  
+
+rm -f testnet/data/plutus-pab.db
+
+testnet/bin/cardano-mixer-pab \
+ --config testnet/pab-config.yml migrate
+  
+testnet/bin/cardano-mixer-pab \
+  --config testnet/pab-config.yml webserver \
+  --passphrase 1234567890
+  
+
+rm -f testnet/data/plutus-pab.db
+
+testnet/bin/cardano-mixer-pab \
+ --config testnet/pab-config.yml migrate
+  
+testnet/bin/cardano-mixer-pab \
+  --config testnet/pab-config.yml webserver \
+  --passphrase 1234567890
+  
+
+rm -f testnet/data/plutus-pab.db
+
+testnet/bin/cardano-mixer-pab \
+ --config testnet/pab-config.yml migrate
+  
+testnet/bin/cardano-mixer-pab \
+  --config testnet/pab-config.yml webserver \
+  --passphrase 1234567890
+  
+
+rm -f testnet/data/plutus-pab.db
+
+testnet/bin/cardano-mixer-pab \
+ --config testnet/pab-config.yml migrate
+  
+testnet/bin/cardano-mixer-pab \
+  --config testnet/pab-config.yml webserver \
+  --passphrase 1234567890
+  
+
+rm -f testnet/data/plutus-pab.db
+
+testnet/bin/cardano-mixer-pab \
+ --config testnet/pab-config.yml migrate
+  
+testnet/bin/cardano-mixer-pab \
+  --config testnet/pab-config.yml webserver \
+  --passphrase 1234567890
+  
+
+rm -f testnet/data/plutus-pab.db
+
+testnet/bin/cardano-mixer-pab \
+ --config testnet/pab-config.yml migrate
+  
+testnet/bin/cardano-mixer-pab \
+  --config testnet/pab-config.yml webserver \
+  --passphrase 1234567890
+  
+
+rm -f testnet/data/plutus-pab.db
+
+testnet/bin/cardano-mixer-pab \
+ --config testnet/pab-config.yml migrate
+  
+testnet/bin/cardano-mixer-pab \
+  --config testnet/pab-config.yml webserver \
+  --passphrase 1234567890
+  
+
+rm -f testnet/data/plutus-pab.db
+
+testnet/bin/cardano-mixer-pab \
+ --config testnet/pab-config.yml migrate
+  
+testnet/bin/cardano-mixer-pab \
+  --config testnet/pab-config.yml webserver \
+  --passphrase 1234567890
+  
+
+
+
+
+
